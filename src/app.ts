0cb1be4ea26b8526b61ed04050560d9dc6bb6457
taianaazevedo <taianazev@gmail.com>
2023-05-31 18:19:23 -0300

    create files

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+import cors from "cors";
+
+import { loadEnv, connectDb, disconnectDB } from "@/config";
+
+
+loadEnv();
+
+const app = express();
+app.use(cors()).use(express.json());
+
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
+
+export default app;
